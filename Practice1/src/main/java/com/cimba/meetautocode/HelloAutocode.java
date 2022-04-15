507a1aff0ca205aee9416a8ce5bc82cba86cc198
Oleksandra Mala <oleksandra.s.mala@gmail.com>
2022-04-16 00:38:56 +0200

    Solved MeetAutocode

---
+++ Practice1/src/main/java/com/cimba/meetautocode/HelloAutocode.java
@@ -0,0 +1,9 @@
+package com.cimba.meetautocode;
+
+public class HelloAutocode {
+    public static void main(String[] args) {
+        //Write a program, printing "Hello, Autocode!"
+        System.out.println("Hello, Autocode!");
+    }
+}
+

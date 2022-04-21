b3d9d107219342f0b50397c016dea332628b1472
Oleksandra Mala <oleksandra.s.mala@gmail.com>
2022-04-21 18:04:08 +0200

    Solved Practice3

---
+++ Practice3/src/main/java/com/cimba/FindMaxInSeq.java
@@ -0,0 +1,38 @@
+package com.cimba;
+import java.util.*;
+
+public class FindMaxInSeq {
+    public static int max() {
+        //TODO do without arraylist, with cycles
+
+        // Put your code here
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<Integer> array = new ArrayList<>();
+        int number;
+        do {
+            number = scanner.nextInt() ;
+            if(number == 0){
+                continue;
+            }else{
+                array.add(number);
+            }
+
+        }while (number != 0);
+
+
+
+        return Collections.max(array);
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println("Test your code here!\n");
+
+        // Get a result of your code
+
+        System.out.println(max());
+    }
+}
+
+
+
